fe2333e927fcb8df1d644685cd69ea87b7090543
rmaghsoudi <romy_mag@outlook.com>
2019-04-25 16:33:47 -0400

    Teamwork makes the dreamwork

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status
+  attr_reader :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : @status = "rejected"
+  end
+
+  def execute_transaction
+    if @sender.balance < @amount
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    elsif self.valid? && self.status == "pending"
+      @receiver.deposit(@amount)
+      @sender.balance -= @amount
+      self.status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      @receiver.balance -= @amount
+      @sender.deposit(@amount)
+      self.status = "reversed"
+    end
+  end
+
 end
